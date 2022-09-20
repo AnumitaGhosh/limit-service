952fa3415ed1298b89592d7150c6d2972b3532ea
AnumitaGhosh <anumita.malhaar@gmail.com>
2022-09-20 16:13:02 +0530

    commit limit service

---
+++ src/main/java/com/example/limit/controller/LimitsConfigurationController.java
@@ -0,0 +1,23 @@
+package com.example.limit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.limit.bean.LimitsConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private LimitsConfiguration limits;
+	
+	@GetMapping("/limits")
+	public LimitsConfiguration returnLimits()
+	{
+		//return new LimitsConfiguration(limits.getMaximum(),limits.getMinimum());
+		return limits;
+		
+	}
+
+}
