15502da833f24f90fac0bd87b047c44c6c14185d
AnumitaGhosh <anumita.malhaar@gmail.com>
2022-09-22 17:13:26 +0530

    Commit from qa

--- src/main/java/com/example/limit/LimitServiceApplication.java
+++ src/main/java/com/example/limit/LimitServiceApplication.java
@@ -8,8 +8,8 @@ public class LimitServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitServiceApplication.class, args);
-		System.out.println("from dev_branch");
-		System.out.println("from master");
+		System.out.println("from qa");
+		
 	}
 
 }
